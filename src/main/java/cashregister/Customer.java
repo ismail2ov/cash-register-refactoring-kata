b6ef4d31bcedbd80dff2766379ec14d733ec8562
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-30 13:49:52 +0100

    Extract amount calculation logic

--- src/main/java/cashregister/Customer.java
+++ src/main/java/cashregister/Customer.java
@@ -32,48 +32,22 @@ public class Customer {
         int numItems = 0;
         String result = "Statement for " + getName() + "\n";
 
-        for (OrderLine each : orderLines) {
+        for (OrderLine orderLine : orderLines) {
             double thisAmount = 0;
             double thisDiscount = 0;
 
-            // determine amounts for each order line
-            switch (each.getProduct().getType()) {
-                case Product.REGULAR:
-                    thisAmount = each.getQuantity() * each.getProduct().getPrice();
-                    break;
-                case Product.PROMOTED:
-                    thisAmount = each.getQuantity() * each.getProduct().getPrice();
-                    break;
-                case Product.SECOND_70_PERCENT_LESS:
-                    if (each.getQuantity() >= 2) {
-                        int itemsToDiscount = each.getQuantity() / 2;
-                        thisDiscount = itemsToDiscount * each.getProduct().getPrice() * 0.7;
-                        thisAmount = each.getQuantity() * each.getProduct().getPrice() - thisDiscount;
-                    } else {
-                        thisAmount = each.getQuantity() * each.getProduct().getPrice();
-                    }
-                    break;
-                case Product.PROMO_3x2:
-                    if (each.getQuantity() >= 3) {
-                        int itemsToDiscount = each.getQuantity() / 3;
-                        thisDiscount = itemsToDiscount * each.getProduct().getPrice();
-                        thisAmount = each.getQuantity() * each.getProduct().getPrice() - thisDiscount;
-                    } else {
-                        thisAmount = each.getQuantity() * each.getProduct().getPrice();
-                    }
-                    break;
-            }
+            thisAmount = amountFor(orderLine);
 
             // add credits for purchasing more than 10 units of regular products
-            if ((each.getProduct().getType() == Product.REGULAR) && each.getQuantity() > 10)
+            if ((orderLine.getProduct().getType() == Product.REGULAR) && orderLine.getQuantity() > 10)
                 accumulatedCredits++;
             // add extra credit for every promoted product purchased
-            if (each.getProduct().getType() == Product.PROMOTED)
-                accumulatedCredits += each.getQuantity();
+            if (orderLine.getProduct().getType() == Product.PROMOTED)
+                accumulatedCredits += orderLine.getQuantity();
 
-            // show figures for each order line
-            result += String.format("\t %s: %d x %.2f = %.2f €\n", each.getProduct().getName(), each.getQuantity(), each.getProduct().getPrice(), thisAmount);
-            numItems += each.getQuantity();
+            // show figures for orderLine order line
+            result += String.format("\t %s: %d x %.2f = %.2f €\n", orderLine.getProduct().getName(), orderLine.getQuantity(), orderLine.getProduct().getPrice(), thisAmount);
+            numItems += orderLine.getQuantity();
             totalAmount += thisAmount;
         }
 
@@ -86,5 +60,39 @@ public class Customer {
         result += String.format("Total amount: %.2f €\n", totalAmount);
 
         return result;
+    }
+
+    private double amountFor(OrderLine orderLine) {
+        double thisAmount = 0;
+        double thisDiscount;
+        // determine amounts for orderLine order line
+        switch (orderLine.getProduct().getType()) {
+            case Product.REGULAR:
+                thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice();
+                break;
+            case Product.PROMOTED:
+                thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice();
+                break;
+            case Product.SECOND_70_PERCENT_LESS:
+                if (orderLine.getQuantity() >= 2) {
+                    int itemsToDiscount = orderLine.getQuantity() / 2;
+                    thisDiscount = itemsToDiscount * orderLine.getProduct().getPrice() * 0.7;
+                    thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice() - thisDiscount;
+                } else {
+                    thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice();
+                }
+                break;
+            case Product.PROMO_3x2:
+                if (orderLine.getQuantity() >= 3) {
+                    int itemsToDiscount = orderLine.getQuantity() / 3;
+                    thisDiscount = itemsToDiscount * orderLine.getProduct().getPrice();
+                    thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice() - thisDiscount;
+                } else {
+                    thisAmount = orderLine.getQuantity() * orderLine.getProduct().getPrice();
+                }
+                break;
+        }
+
+        return thisAmount;
     }
 }
