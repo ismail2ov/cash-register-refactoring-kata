faa0f707b17720386e50c8db29a3454eec3a037b
Ismail Ahmedov <iahmedov@paradigmadigital.com>
2021-11-30 14:06:51 +0100

    Extract amount calculation logic to Product

--- src/main/java/cashregister/OrderLine.java
+++ src/main/java/cashregister/OrderLine.java
@@ -15,37 +15,8 @@ public class OrderLine {
     }
 
     public double amountFor() {
-        double result = 0;
-        double thisDiscount;
-        // determine amounts for orderLine order line
-        switch (getProduct().getType()) {
-            case Product.REGULAR:
-                result = getQuantity() * getProduct().getPrice();
-                break;
-            case Product.PROMOTED:
-                result = getQuantity() * getProduct().getPrice();
-                break;
-            case Product.SECOND_70_PERCENT_LESS:
-                if (getQuantity() >= 2) {
-                    int itemsToDiscount = getQuantity() / 2;
-                    thisDiscount = itemsToDiscount * getProduct().getPrice() * 0.7;
-                    result = getQuantity() * getProduct().getPrice() - thisDiscount;
-                } else {
-                    result = getQuantity() * getProduct().getPrice();
-                }
-                break;
-            case Product.PROMO_3x2:
-                if (getQuantity() >= 3) {
-                    int itemsToDiscount = getQuantity() / 3;
-                    thisDiscount = itemsToDiscount * getProduct().getPrice();
-                    result = getQuantity() * getProduct().getPrice() - thisDiscount;
-                } else {
-                    result = getQuantity() * getProduct().getPrice();
-                }
-                break;
-        }
-
-        return result;
+        int quantity = getQuantity();
+        return product.amountFor(quantity);
     }
 
     int creditsFor() {
--- src/main/java/cashregister/Product.java
+++ src/main/java/cashregister/Product.java
@@ -15,4 +15,38 @@ public class Product {
     private String name;
     private double price;
     private int type;
+
+    double amountFor(int quantity) {
+        double result = 0;
+        double thisDiscount;
+        // determine amounts for orderLine order line
+        switch (this.getType()) {
+            case REGULAR:
+                result = quantity * this.getPrice();
+                break;
+            case PROMOTED:
+                result = quantity * this.getPrice();
+                break;
+            case SECOND_70_PERCENT_LESS:
+                if (quantity >= 2) {
+                    int itemsToDiscount = quantity / 2;
+                    thisDiscount = itemsToDiscount * this.getPrice() * 0.7;
+                    result = quantity * this.getPrice() - thisDiscount;
+                } else {
+                    result = quantity * this.getPrice();
+                }
+                break;
+            case PROMO_3x2:
+                if (quantity >= 3) {
+                    int itemsToDiscount = quantity / 3;
+                    thisDiscount = itemsToDiscount * this.getPrice();
+                    result = quantity * this.getPrice() - thisDiscount;
+                } else {
+                    result = quantity * this.getPrice();
+                }
+                break;
+        }
+
+        return result;
+    }
 }
